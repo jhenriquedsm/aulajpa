b3869806d496819960d8fc9134c71ddb7556ebb5
jhenriquedsm <jhsilvamata@gmail.com>
2025-01-06 12:02:45 -0300

    searching for data with jpa

--- src/main/java/jhenriquedsm/application/Program.java
+++ src/main/java/jhenriquedsm/application/Program.java
@@ -8,17 +8,20 @@ import javax.persistence.Persistence;
 
 public class Program {
     public static void main(String[] args) {
+        /*
         Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@email.com");
         Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@email.com");
         Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+        */
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
         System.out.println("Ready!");
+        em.close();
+        emf.close();
     }
 }
